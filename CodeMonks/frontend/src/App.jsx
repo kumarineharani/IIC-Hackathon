b76a72ef09eef644e9eb4c9408b3783b554c3836
Abhigyan <abhigyansrivastav7@gmail.com>
2025-01-28 15:42:55 +0530

    wrapped up

---
+++ CodeMonks/frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import ImageUploadAndRecognition from './components/ImageUploadAndRecognition'
+function App() {
+ 
+  return (
+    <>  
+   <ImageUploadAndRecognition/>
+    </>
+  )
+}
+
+export default App
